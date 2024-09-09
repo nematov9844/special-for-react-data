7a9f28b65cff9c056247c0dab2cb0e83de3d524f
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-09-09 20:53:16 +0500

    main

--- api/users.js
+++ api/users.js
@@ -1,19 +1,24 @@
+
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); 
+
 app.use(express.json());
 
-let users = [];
+let users = [
 
-// /api/data endpointini yaratish
-app.get('/api/data', (req, res) => {
+];
+
+
+app.get('/api/users', (req, res) => {
     res.json(users);
 });
 
-app.post('/api/data', (req, res) => {
+app.post('/api/users', (req, res) => {
     const user = req.body;
     users.push(user);
     res.status(201).json(user);
